38874dae9243264d30eadab828d5fd68cd7b062b
Marc <marc.wilk@colorado.edu>
2019-01-11 15:34:11 -0700

    added quantity/product/submit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import './App.css';
 import CartHeader from './components/CartHeader.jsx';
 import CartItems from './components/CartItems.jsx';
 import CartFooter from './components/CartFooter.jsx';
+import AddItem from './components/AddItem.jsx';
 
 class App extends Component {
+  state = {
+    products: [
+      { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+      { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+      { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+      { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+      { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+      { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+      { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+      { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+      { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+    ]
+  }
   render() {
     const cartItemsList = [
       { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
@@ -16,11 +30,11 @@ class App extends Component {
       <div>
         <CartHeader />
         <CartItems cartItemsList={cartItemsList} />
+        <AddItem products={this.state.products} />
         <CartFooter copyright={2019} />
       </div>
     )
   }
-
 }
 
 export default App;
