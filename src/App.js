6db2aca952d566307bd910e9d528a504854c99ae
Marc <marc.wilk@colorado.edu>
2019-01-11 13:04:32 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import CartHeader from './components/CartHeader.jsx';
 import CartItems from './components/CartItems.jsx';
-import CartItem from './components/CartItem.jsx';
 import CartFooter from './components/CartFooter.jsx';
-
-const cartItemsList = [
-  { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-  { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-  { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
-]
 
 class App extends Component {
   render() {
+    const cartItemsList = [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ]
     return (
       <div>
-        <CartHeader/>
-        <CartItems/>
-        <CartItem/>
-        <CartFooter copyright={2019}/>
+        <CartHeader />
+        <CartItems items={cartItemsList} />
+        <CartFooter copyright={2019} />
       </div>
     )
   }
+
 }
 
 export default App;
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const CartItem = () => (
-  <div class="collection-item">
-    <div class="row">
-      <div class="col-md-8">Mediocre Iron Watch</div>
-      <div class="col-md-2">$3.99</div>
-      <div class="col-md-2">1</div>
+const CartItem = () => {
+  return (
+    <div class="collection-item">
+      <div class="row">
+        <div class="col-md-8">Mediocre Iron Watch</div>
+        <div class="col-md-2">$3.99</div>
+        <div class="col-md-2">1</div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default CartItem;
